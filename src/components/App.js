7da198e86e01717a6d829c392f466c89d98839ce
Garry Kim <moduedu@gamil.com>
2018-12-20 15:26:27 +0900

    components seting Initialize

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Counter from './Counter';
+
+class App extends Component {
+
+    static defaultProps={
+
+ };
+
+render() {
+     return (
+        <div>
+              <Counter/>  
+        </div>
+        );
+    }
+}
+
+export default App;
